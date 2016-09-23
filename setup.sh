fb3e92919ece8ee721392f5d015c31dde3f567bc
Chang-Hong Hsu <hsuch@umich.edu>
2016-09-23 09:51:22 -0400

    updating setup.sh so that it directly holds gitlab-ce during the setup process

--- setup.sh
+++ setup.sh
@@ -1,14 +1,20 @@
 link_path="/usr/local/bin"
 target_path=`pwd`
+hold_package="gitlab-ce"
 exec_file="upgrade_gitlab-ce.sh"
 
 symlink_link="$link_path/$exec_file"
 symlink_target="$target_path/$exec_file"
 
+
+
+
 echo "Creating symbolic link for $exec_file ..."
 echo "symlink: $symlink_link"
 echo "target : $symlink_target"
- 
+echo ""
+
+
 if [ -f $symlink_link ]; then
     echo "Error: symlink $symlink_link exists!"
     echo "It points to `readlink -f $symlink_link`";
@@ -18,6 +24,13 @@ elif [ ! -f $symlink_target ]; then
     echo "Do nothing";
 else 
     sudo ln -fs $symlink_target $symlink_link
+    echo "symlink created."
+    echo ""
+
+    echo "Setting up: holding package $hold_package ..."
+    hold_cmd="sudo apt-mark hold $hold_package"
+    echo $hold_cmd
+    eval $hold_cmd
 fi
 
 
